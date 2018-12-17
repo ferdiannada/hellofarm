67a5f0e9dfaf230e9e9af36fab075be6b4c2d267
Zainur Rofan <seesaajeen2@gmail.com>
2018-12-17 09:45:14 +0700

    Anyartoko

---
+++ toko/konfirmasipembayaran.php
@@ -0,0 +1,257 @@
+<?php include("../koneksi.php");
+  session_start();
+ ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <title>HelloFarm</title>
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+  <meta content="" name="keywords">
+  <meta content="" name="description">
+
+  <!-- Favicons -->
+  <link href="../img/favicon.png" rel="icon">
+  <link href="../img/apple-touch-icon.png" rel="apple-touch-icon">
+
+  <!-- Google Fonts -->
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,700,700i|Poppins:300,400,500,700" rel="stylesheet">
+
+  <!-- Libraries CSS Files -->
+  <link href="../lib/font-awesome/css/font-awesome.min.css" rel="stylesheet">
+  <link href="../lib/animate/animate.min.css" rel="stylesheet">
+
+  <!-- Main Stylesheet File Header -->
+  <link href="../akun/profil/css/style.css" rel="stylesheet">
+
+  <!-- Bootstrap CSS File -->
+  <link href="../lib/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+  <!-- Main Stylesheet File -->
+  <link href="css/style.css" rel="stylesheet">
+  <link href="css/nav.css" rel="stylesheet">
+  <link href="css/form-validation.css" rel="stylesheet">
+
+  <!-- Modal -->
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/modernizr/2.8.3/modernizr.min.js" type="text/javascript"></script>
+  <link href='https://fonts.googleapis.com/css?family=PT+Sans:400,700' rel='stylesheet' type='text/css'>
+  <link rel="stylesheet" href="https://s3-us-west-2.amazonaws.com/s.cdpn.io/148866/reset.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/meyer-reset/2.0/reset.min.css">
+  <link rel="stylesheet" href="../login/css/style.css">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
+</head>
+
+<body>
+
+  <!--==========================
+  Header
+  ============================-->
+  <header id="header">
+    <div class="container">
+
+      <div id="logo" class="pull-left">
+          <a href="../"><img src="../img/logo.png" alt="" title="" /></img></a>
+      </div>
+
+      <nav id="nav-menu-container">
+        <ul class="nav-menu">
+          <li><a href="../#hero">Beranda</a></li>
+          <li><a href="../#about">Tentang</a></li>
+          <li><a href="../#portfolio">Fitur</a></li>
+          <li class="menu-active"><a href="">Toko</a></li>
+          <li><a href="../artikel">Artikel</a></li>
+          <li><a href="#contact">Hubungi</a></li>
+          <?php if (isset($_SESSION['status'])) { ?>
+            <li class="menu-has-children"><a href="#">Selamat Datang <?php echo $_SESSION['username']; ?></a>
+              <ul>
+
+                <li><a href="../akun/profil">Data Akun</a></li>
+                <li><a href="../prosesLogout.php">Keluar</a></li>
+              </ul>
+            </li>
+
+          <?php }else {?>
+            <li class="menu-has-children"><a href="#">Akun</a>
+              <ul>
+                  <li><a href="" data-toggle="modal" data-target="#nuLogin">Login</a></li>
+                  <li><a href="" data-toggle="modal" data-target="#nuRegister">Register</a></li>
+              </ul>
+            </li>
+        <?php } ?>
+        </ul>
+      </nav><!-- #nav-menu-container -->
+    </div>
+  </header><!-- #header -->
+
+  <?php
+        include("login.php");
+        include("registrasi.php");
+    ?>
+
+  <!--==========================
+    Hero Section
+  ============================-->
+
+  <main id="main">
+
+        <!-- Page Content -->
+    <div class="container" style="margin-top: 150px; margin-left: 250px;">
+    <div class="row">
+      <div class="text-left">
+          <h1>Detail Pembayaran</h1>
+      </div>
+        <div class="col-xs-10 col-sm-10 col-xs-offset-1 col-sm-offset-1 ">
+            <div class="row">
+                <div class="col-xs-6 col-sm-6 col-md-6">
+                    <address>
+                        <strong>Elf Cafe</strong>
+                        <br>
+                        2135 Sunset Blvd
+                        <br>
+                        Los Angeles, CA 90026
+                        <br>
+                        <abbr title="Phone">P:</abbr> (213) 484-6829
+                    </address>
+                </div>
+                <div class="col-xs-6 col-sm-6 col-md-6 text-right">
+                    <p>
+                        <em>Date: 1st November, 2013</em>
+                    </p>
+                    <p>
+                        <em>Pemesanan #: 34522677W</em>
+                    </p>
+                </div>
+            </div>
+            <div class="row">
+                <div class="text-center">
+                    <h2>Rincian pemesanan</h2>
+                </div>
+                </span>
+                <table class="table table-hover">
+                    <thead>
+                        <tr>
+                            <th>Produk</th>
+                            <th class="text-center">Kuantitas</th>
+                            <th class="text-center">Harga</th>
+                            <th class="text-center">Total</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td class=""><em>Baked Rodopa Sheep Feta</em></h4></td>
+                            <td class="" style="text-align: center"> 2 </td>
+                            <td class=" text-center">$13</td>
+                            <td class=" text-center">$26</td>
+                        </tr>
+                        <tr>
+                            <td class=""><em>Lebanese Cabbage Salad</em></h4></td>
+                            <td class="" style="text-align: center"> 1 </td>
+                            <td class=" text-center">$8</td>
+                            <td class=" text-center">$8</td>
+                        </tr>
+                        <tr>
+                            <td class=""><em>Baked Tart with Thyme and Garlic</em></h4></td>
+                            <td class="" style="text-align: center"> 3 </td>
+                            <td class=" text-center">$16</td>
+                            <td class=" text-center">$48</td>
+                        </tr>
+                        <tr>
+                            <td>   </td>
+                            <td>   </td>
+                            <td class="text-right">
+                            <p>
+                                <strong>Subtotal: </strong>
+                            </p>
+                            <p>
+                                <strong>Shipping: </strong>
+                            </p></td>
+                            <td class="text-center">
+                            <p>
+                                <strong>$6.94</strong>
+                            </p>
+                            <p>
+                                <strong>$6.94</strong>
+                            </p></td>
+                        </tr>
+                        <tr>
+                            <td>   </td>
+                            <td>   </td>
+                            <td class="text-right"><h4><strong>Total: </strong></h4></td>
+                            <td class="text-center text-danger"><h4><strong>$31.53</strong></h4></td>
+                        </tr>
+                    </tbody>
+                </table>
+                <div class="col-xs-6 col-sm-6 col-md-6">
+                    <address>
+                        <strong>Silahkan melakukan pembayaran ke rekening a/n Ferdian Nada:</strong>
+                        <br>
+                        Bank BRI :
+
+                        64761487634
+                        <br>
+                        Bank BCA :
+
+                        64761487634
+                        <br>
+                        Bank Mandiri :
+
+                        64761487634
+                        <br>
+                    </address>
+                </div>
+                <button type="button" class="btn btn-success btn-lg btn-block">
+                    Pesan Sekarang   <span class="glyphicon glyphicon-chevron-right"></span>
+                </button></td>
+            </div>
+        </div>
+      </div>
+
+  </main>
+
+  <!--==========================
+    Footer
+  ============================-->
+  <footer id="footer" style="">
+    <div class="footer-top">
+      <div class="container">
+
+      </div>
+    </div>
+
+    <div class="container">
+      <div class="copyright">
+        &copy; Copyright <strong>Hello Farm</strong>. All Rights Reserved
+      </div>
+      <div class="credits">
+        Designed by <a href="https://bootstrapmade.com/">Ropan</a>
+      </div>
+    </div>
+  </footer><!-- #footer -->
+
+  <a href="#" class="back-to-top"><i class="fa fa-chevron-up"></i></a>
+
+  <!-- JavaScript Libraries -->
+  <script src="../lib/jquery/jquery.min.js"></script>
+  <script src="../lib/jquery/jquery-migrate.min.js"></script>
+  <script src="../lib/bootstr../bootstrap.bundle.min.js"></script>
+  <script src="../lib/easing/easing.min.js"></script>
+  <script src="../lib/wow/wow.min.js"></script>
+  <script src="../lib/waypoints/waypoints.min.js"></script>
+  <script src="../lib/counterup/counterup.min.js"></script>
+  <script src="../lib/superfish/hoverIntent.js"></script>
+  <script src="../lib/superfish/superfish.min.js"></script>
+
+  <!-- Contact Form JavaScript File -->
+  <script src="../contactform/contactform.js"></script>
+
+  <!-- Template Main Javascript File -->
+  <script src="../js/main.js"></script>
+
+  <!-- Modal Login -->
+  <script src='http://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js'></script>
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
+
+</body>
+</html>
